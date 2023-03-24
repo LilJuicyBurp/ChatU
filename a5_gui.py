7d13d97da524e651f178085d603c68f7797c3d47
Steven Deng <sdeng5@uci.edu>
2023-03-24 16:06:54 -0700

    Started working on a starter menu

--- a5_gui.py
+++ a5_gui.py
@@ -2,7 +2,7 @@
 # sdeng5@uci.edu
 # 47704456
 import tkinter as tk
-from tkinter import ttk, messagebox
+from tkinter import ttk, simpledialog, messagebox
 from typing import Text
 import pathlib
 import time
@@ -320,8 +320,22 @@ class MainApp(tk.Frame):
         self.body.reset_ui()
         self.root.destroy()
 
+    def start_menu(self):
+        starter_frame = tk.Frame(master=self.root,width=360,
+                                 height=240, bg='light blue')
+        starter_frame.place(anchor='center')
+        msg = 'Welcome To DSP Messenger'
+        msg_by = 'Created by: Steven Deng'
+        welcome_msg = tk.Text(starter_frame, width=360, height=120)
+        welcome_msg.tag_configure('entry-mid', justify='center',
+                                  font='"Comic Sans MS" 13 bold')
+        welcome_msg.pack(anchor='center', expand=True)
+        welcome_msg.insert(1.0, msg + '\n', 'entry-mid')
+        welcome_msg.insert(1.0, msg_by + '\n', 'entry-mid')
+        starter_frame.pack()
+
     def _draw(self):
-        # Build a menu and add it to the root frame.
+        self.start_menu()
         menu_bar = tk.Menu(self.root)
         self.root['menu'] = menu_bar
         menu_file = tk.Menu(menu_bar)
@@ -337,9 +351,6 @@ class MainApp(tk.Frame):
                                   command=self.add_contact)
         settings_file.add_command(label='Configure Profile',
                                   command=self.configure_server)
-
-        # The Body and Footer classes must be initialized and
-        # packed into the root window.
         self.body = Body(self.root,
                          recipient_selected_callback=self.recipient_selected)
         self.body.pack(fill=tk.BOTH, side=tk.TOP, expand=True)
@@ -348,36 +359,12 @@ class MainApp(tk.Frame):
 
 
 if __name__ == "__main__":
-    # All Tkinter programs start with a root window. We will name ours 'main'.
     main = tk.Tk()
-
-    # 'title' assigns a text value to the Title Bar area of a window.
     main.title("ICS 32 Distributed Social Messenger")
-
-    # This is just an arbitrary starting point. You can change the value
-    # around to see how the starting size of the window changes.
     main.geometry("720x480")
-
-    # adding this option removes some legacy behavior with menus that
-    # some modern OSes don't support. If you're curious, feel free to comment
-    # out and see how the menu changes.
     main.option_add('*tearOff', False)
-
-    # Initialize the MainApp class, which is the starting point for the
-    # widgets used in the program. All of the classes that we use,
-    # subclass Tk.Frame, since our root frame is main, we initialize
-    # the class with it.
     app = MainApp(main)
-
-    # When update is called, we finalize the states of all widgets that
-    # have been configured within the root frame. Here, update ensures that
-    # we get an accurate width and height reading based on the types of widgets
-    # we have used. minsize prevents the root window from resizing too small.
-    # Feel free to comment it out and see how the resizing
-    # behavior of the window changes.
     main.update()
     main.minsize(main.winfo_width(), main.winfo_height())
-    # And finally, start up the event loop for the program (you can find
-    # more on this in lectures of week 9 and 10).
     main.after(5000, app.check_new)
     main.mainloop()
